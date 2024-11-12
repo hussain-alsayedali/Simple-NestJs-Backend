52c82c20705a28f7c3a867a82e43de1179c1d900
Hussain Asim Al Sayed Ali <hussain.alsayedali@gmail.com>
2024-11-12 18:25:17 +0300

    nestjs guards works with user

--- src/user-auth/user-auth.module.ts
+++ src/user-auth/user-auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserAuthController } from './user-auth.controller';
 import { UserAuthService } from './user-auth.service';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [UserAuthController],
-  providers: [UserAuthService],
+  providers: [UserAuthService, JwtStrategy],
 })
 export class AuthModule {}
